995b337ce385eb54df68dfdbaa120a7755f51a62
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2021-11-29 12:48:19 +0530

    added navbar

--- React/demo-mcanvis/src/App.js
+++ React/demo-mcanvis/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Navbar from './Navbar'
+import Header from "./Header"
 
 let App = () => 
     <div>
-        <Navbar/>
+        <Header/>
         {/* <Main/> */}
         {/* <Footer/> */}
     </div>
--- React/demo-mcanvis/src/Navbar.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-let Navbar = () =>
-    <h1>Hello world</h1>
-
-export default Navbar;
